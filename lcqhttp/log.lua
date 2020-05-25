820ef554c19f46069e233a0a642d1315b289e73b
midori <cleoold@gmail.com>
2020-05-24 20:30:48 -0400

    log rework

--- lcqhttp/log.lua
+++ lcqhttp/log.lua
@@ -2,15 +2,16 @@ local lcqhttp = {
     util = require 'lcqhttp.util'
 }
 
-local log = function(fmt, ...)
-    print(lcqhttp.util.timestr()..' '..fmt:format(...))
+local log = function(pre, fmt, ...)
+    fmt = fmt or 'unknown'
+    print(lcqhttp.util.timestr()..' '..pre..fmt:format(...))
 end
 
-local lerror = function(fmt, ...) log('ERROR: '..fmt, ...) end
+local lerror = function(fmt, ...) log('ERROR: ', fmt, ...) end
 
-local lwarn = function(fmt, ...) log('WARNING: '..fmt, ...) end
+local lwarn = function(fmt, ...) log('WARNING: ', fmt, ...) end
 
-local ldebug = function(fmt, ...) log('DEBUG: '..fmt, ...) end
+local ldebug = function(fmt, ...) log('DEBUG: ', fmt, ...) end
 
 return {
     log = log,
